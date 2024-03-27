93be35f49dfd3d7b62fe8fedf23997bb33fd07ec
caio <caioefg@gmail.com>
2024-03-27 13:45:51 -0300

    Refatorando

--- src/style/index.ts
+++ src/style/index.ts
@@ -33,17 +33,6 @@ export const Label = styled.label`
   margin-right: 10px;
 `;
 
-export const Input = styled.input`
-  border-radius: 5px;
-  border: 1px solid black;
-  padding: 0.5em;
-  width: 100%;
-  &:focus {
-    outline: none;
-    box-shadow: 0px 0px 2px red;
-  }
-`;
-
 export const FlexContainer = styled.div<{
   justify?:
     | 'space-between'
